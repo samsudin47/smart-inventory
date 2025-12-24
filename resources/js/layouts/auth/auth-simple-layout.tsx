94167e9606052e35a91f665b5334e1122b377c18
PDS_samsudin <mohamad.samsudin@pds.id>
2025-12-24 11:20:40 +0700

    fix: fixing background login and register

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -11,7 +11,9 @@ interface AuthLayoutProps {
 
 export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+        <div 
+            className="auth-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+        >
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
